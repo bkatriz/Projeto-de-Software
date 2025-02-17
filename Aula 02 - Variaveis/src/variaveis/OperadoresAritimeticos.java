fb148b815d4096034f8b79ccb62f0f4264732e0a
KaioV05 <kaioviniciusbdc@gmail.com>
2025-02-17 10:38:23 -0300

    02

---
+++ Aula 02 - Variaveis/src/variaveis/OperadoresAritimeticos.java
@@ -0,0 +1,26 @@
+package variaveis;
+
+import java.util.Scanner;
+
+public class OperadoresAritimeticos {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		String nome; 
+		System.out.print("Digite seu nome:");
+		nome = sc.next();
+		System.out.println(nome);
+		
+		System.out.println("Digite sua idade:");
+		int idade = sc.nextInt();
+		
+	
+		
+		sc.close();
+
+	}
+
+}
---
+++ Aula 02 - Variaveis/src/variaveis/TiposDados.java
@@ -0,0 +1,10 @@
+package variaveis;
+
+public class TiposDados {
+
+	public static void main(String[] args) {
+		int idade = 30;
+
+	}
+
+}

0f7ba2038e0964ac1cf90769c3d04e91e84e2f2f
KaioV05 <kaioviniciusbdc@gmail.com>
2025-02-17 10:38:12 -0300

    Aula 2 Variáveis

---
+++ Aula2 Variáveis/src/Variáveis/Funções.java
@@ -0,0 +1,21 @@
+package Variáveis;
+
+public class Funções {
+
+	public static void main(String[] args) {
+		
+		//Operador Potência
+		double potencia = Math.pow(3, 2);
+		System.out.println(potencia);
+		
+		//Operador Raiz Quadrada
+		double raizQuadrada = Math.sqrt(16);
+		System.out.println(raizQuadrada);
+		
+		//Número aleatório
+		double numeroAleatorio = Math.random();
+		System.out.printf("%.2f", numeroAleatorio);
+		
+	}
+
+}
